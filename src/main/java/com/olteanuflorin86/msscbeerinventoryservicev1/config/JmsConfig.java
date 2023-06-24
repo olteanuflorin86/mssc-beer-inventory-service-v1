38f8cdc54d0f3e2b4521edddb3a38648332854e3
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-24 19:44:27 +0300

    Handle Allocation Request

--- src/main/java/com/olteanuflorin86/msscbeerinventoryservicev1/config/JmsConfig.java
+++ src/main/java/com/olteanuflorin86/msscbeerinventoryservicev1/config/JmsConfig.java
@@ -12,6 +12,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JmsConfig {
 	
 	public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+	public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
 
 	@Bean // Serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
---
+++ src/main/java/com/olteanuflorin86/msscbeerinventoryservicev1/services/AllocationListener.java
@@ -0,0 +1,44 @@
+package com.olteanuflorin86.msscbeerinventoryservicev1.services;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import com.olteanuflorin86.brewery.model.events.AllocateOrderRequest;
+import com.olteanuflorin86.brewery.model.events.AllocateOrderResult;
+import com.olteanuflorin86.msscbeerinventoryservicev1.config.JmsConfig;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AllocationListener {
+   private final AllocationService allocationService;
+   private final JmsTemplate jmsTemplate;
+
+   @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+   public void listen(AllocateOrderRequest request){
+       AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
+       builder.beerOrderDto(request.getBeerOrderDto());
+
+       try{
+           Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+
+           if (allocationResult){
+               builder.pendingInventory(false);
+           } else {
+               builder.pendingInventory(true);
+           }
+       } catch (Exception e){
+           log.error("Allocation failed for Order Id:" + request.getBeerOrderDto().getId());
+           builder.allocationError(true);
+       }
+
+       jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
+               builder.build());
+
+   }
+
+}
