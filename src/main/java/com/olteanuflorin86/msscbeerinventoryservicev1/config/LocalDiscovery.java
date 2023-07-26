b53976abaccc19d77f189ae2dad2302cf769d616
Florin Olteanu <olteanuflorin86@gmail.com>
2023-07-26 17:46:44 +0300

    adding client

---
+++ src/main/java/com/olteanuflorin86/msscbeerinventoryservicev1/config/LocalDiscovery.java
@@ -0,0 +1,12 @@
+package com.olteanuflorin86.msscbeerinventoryservicev1.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@EnableDiscoveryClient
+@Configuration
+public class LocalDiscovery {
+
+}
